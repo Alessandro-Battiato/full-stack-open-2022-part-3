637c32807936a767b6f37e7d221469a760186057
Alessandro-Battiato <workstuff32@outlook.it>
2022-10-30 19:20:16 +0100

    correction commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { response } = require("express");
 
 app.use(cors());
 app.use(express.json());
@@ -17,16 +18,16 @@ app.post("/api/notes", (request, response) => {
       error: "content missing",
     });
   }
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+  response.json(note);
 });
-
-const note = {
-  content: body.content,
-  important: body.important || false,
-  date: new Date(),
-  id: generateId(),
-};
-
-notes = notes.concat(note);
 
 let notes = [
   {
