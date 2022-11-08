6a08b4b1fa7131fc1510839c610ee4f357b87646
Alessandro-Battiato <workstuff32@outlook.it>
2022-11-08 19:37:23 +0100

    final commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: "eslint:recommended",
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    semi: ["error", "never"],
+    eqeqeq: "error",
+    "no-console": 0,
+  },
+};
